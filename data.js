7918f6f96e54584e17a727db9c950739f9daa132
Carter Rose <CarterRose@Carters-MacBook-Pro.local>
2019-03-22 12:54:20 -0400

    adding benefit info to product pages

--- data.js
+++ data.js
@@ -5,7 +5,8 @@ const data = {
         name: "BLACK AROMA GEM DIFFUSER",
         slogan: "Shine brightly, breathe deeply.",
         features: "115 ml water tank that diffuses approximately 450 sq ft. Mists for approximately 4.5 hours continuous | 9 hours intermittent (30 sec on/30 sec off). Decorative glass cover. Automatic shut-off and whisper-quiet operation.",
-        price: "$79.95"
+        price: "$79.95",
+        benefits: "BENEFITS:<br><br>Purifies and humidifies while infusing your air with the healing power of plants.<br><br>Releases negative ions to reduce dust, pet dander and other airborne allergens (like dust mite matter).<br><br>Refreshes the air in your space using 100% natural essential oil blends.<br><br>Preserves the full integrity and properties of your essential oil blends using a heat-free system.<br><br>Gives your atmosphere the same refreshing feeling as standing near a waterfall."
     },
 
     stone: {
@@ -13,7 +14,8 @@ const data = {
         name: "AROMA OM STONE EDITION DIFFUSER",
         slogan: "Your wellness is etched in stone.",
         features: "Heat-free to preserve the integrity of your oils for maximum benefit. May also be used as a humidifier. Soft LED light. Energy-efficient, whisper-quiet operation that features an auto shut-off option. Decorative cement cover.",
-        price: "$89.95"
+        price: "$89.95",
+        benefits: "BENEFITS:<br><br>Purifies and humidifies while infusing your air with the healing power of plants.<br><br>Releases negative ions to reduce dust, pet dander and other airborne allergens (like dust mite matter).<br><br>Refreshes the air in your space using 100% natural essential oil blends.<br><br>Preserves the full integrity and properties of your essential oil blends using a heat-free system.<br><br>Gives your atmosphere the same refreshing feeling as standing near a waterfall."
     },
 
     time: {
@@ -21,7 +23,8 @@ const data = {
         name: "WHITE AROMA TIME DIFFUSER",
         slogan: "It's about time.",
         features: "Two tanks and a timer you can pre-set to start automatically morning and night means you can have wellness around the clock. Inspired by the body’s natural rhythms, fill the East tank with an energizing blend to rise, and the West tank with a relaxing blend for rest.",
-        price: "$189.95"
+        price: "$189.95",
+        benefits: "BENEFITS:<br><br>Purifies and humidifies while infusing your air with the healing power of plants.<br><br>Releases negative ions to reduce dust, pet dander and other airborne allergens (like dust mite matter).<br><br>Refreshes the air in your space using 100% natural essential oil blends.<br><br>Preserves the full integrity and properties of your essential oil blends using a heat-free system.<br><br>Gives your atmosphere the same refreshing feeling as standing near a waterfall."
     },
 
     gold: {
@@ -29,7 +32,8 @@ const data = {
         name: "AROMA OM GOLD MARBLE DIFFUSER",
         slogan: "Find your balance.",
         features: "100 ml water tank that diffuses approximately 400 sq ft. Mists for approximately 3 hours continuous | 6 hours intermittent (30 sec on/30 sec off). Sleek polyresin cover and an optional LED light feature. Automatic shut-off and whisper-quiet operation.",
-        price: "$89.95"
+        price: "$89.95",
+        benefits: "BENEFITS:<br><br>Purifies and humidifies while infusing your air with the healing power of plants.<br><br>Releases negative ions to reduce dust, pet dander and other airborne allergens (like dust mite matter).<br><br>Refreshes the air in your space using 100% natural essential oil blends.<br><br>Preserves the full integrity and properties of your essential oil blends using a heat-free system.<br><br>Gives your atmosphere the same refreshing feeling as standing near a waterfall."
     },
 };
 
