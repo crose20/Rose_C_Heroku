f2c3bd7f5b4ded571aaddfa08aa53494dbfb13a5
Carter Rose <CarterRose@Carters-MacBook-Pro.local>
2019-03-22 11:55:39 -0400

    fixing errors

--- data.js
+++ data.js
@@ -1,21 +1,21 @@
 const data = {
     
-    standard: {
+    gem: {
         productpic: "diffuser-gem.jpg",
         name: "BLACK AROMA GEM DIFFUSER",
         desc: "Shine brightly, breathe deeply.",
         info: "115 ml water tank that diffuses approximately 450 sq ft. Mists for approximately 4.5 hours continuous | 9 hours intermittent (30 sec on/30 sec off). Decorative glass cover. Automatic shut-off and whisper-quiet operation."
     },
 
-    traditional: {
+    stone: {
         productpic: "diffuser-stone.jpg",
         name: "AROMA OM STONE EDITION DIFFUSER",
         desc: "Your wellness is etched in stone.",
         info: "Heat-free to preserve the integrity of your oils for maximum benefit. May also be used as a humidifier. Soft LED light. Energy-efficient, whisper-quiet operation that features an auto shut-off option. Decorative cement cover."
     },
 
-    classic: {
-        productpic: "diffuser-gem.jpg",
+    time: {
+        productpic: "diffuser-time.jpg",
         name: "WHITE AROMA TIME DIFFUSER",
         desc: "It's about time.",
         info: "Two tanks and a timer you can pre-set to start automatically morning and night means you can have wellness around the clock. Inspired by the body’s natural rhythms, fill the East tank with an energizing blend to rise, and the West tank with a relaxing blend for rest."
