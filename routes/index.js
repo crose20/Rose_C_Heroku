98e1bf99bdecc325b0646ec20c89cee9d798826d
Carter Rose <CarterRose@Carters-MacBook-Pro.local>
2019-03-22 11:43:44 -0400

    adding initial files

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+const data = require('../data');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Saje Diffusers | Invite the power of nature into your décor with an essential oil diffuser. Fill any space, big or small with wellness-enhancing oil blends.' });
+  
+});
+
+/* A dynamic placeholder */
+router.get('/:user', function(req, res, next) {
+  console.log('data:',data);
+  console.log('user:',req.params.user);
+  res.render('desc', data[req.params.user]);
+});
+
+module.exports = router;
+
