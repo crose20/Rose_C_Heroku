facfd05b84fbaa8a38812881f11e64860b003b55
Carter Rose <CarterRose@Carters-MacBook-Pro.local>
2019-03-22 12:30:31 -0400

    adding product price info

--- data.js
+++ data.js
@@ -4,28 +4,32 @@ const data = {
         productpic: "diffuser-gem.jpg",
         name: "BLACK AROMA GEM DIFFUSER",
         slogan: "Shine brightly, breathe deeply.",
-        features: "115 ml water tank that diffuses approximately 450 sq ft. Mists for approximately 4.5 hours continuous | 9 hours intermittent (30 sec on/30 sec off). Decorative glass cover. Automatic shut-off and whisper-quiet operation."
+        features: "115 ml water tank that diffuses approximately 450 sq ft. Mists for approximately 4.5 hours continuous | 9 hours intermittent (30 sec on/30 sec off). Decorative glass cover. Automatic shut-off and whisper-quiet operation.",
+        price: "$79.95"
     },
 
     stone: {
         productpic: "diffuser-stone.jpg",
         name: "AROMA OM STONE EDITION DIFFUSER",
         slogan: "Your wellness is etched in stone.",
-        features: "Heat-free to preserve the integrity of your oils for maximum benefit. May also be used as a humidifier. Soft LED light. Energy-efficient, whisper-quiet operation that features an auto shut-off option. Decorative cement cover."
+        features: "Heat-free to preserve the integrity of your oils for maximum benefit. May also be used as a humidifier. Soft LED light. Energy-efficient, whisper-quiet operation that features an auto shut-off option. Decorative cement cover.",
+        price: "$89.95"
     },
 
     time: {
         productpic: "diffuser-time.jpg",
         name: "WHITE AROMA TIME DIFFUSER",
         slogan: "It's about time.",
-        features: "Two tanks and a timer you can pre-set to start automatically morning and night means you can have wellness around the clock. Inspired by the body’s natural rhythms, fill the East tank with an energizing blend to rise, and the West tank with a relaxing blend for rest."
+        features: "Two tanks and a timer you can pre-set to start automatically morning and night means you can have wellness around the clock. Inspired by the body’s natural rhythms, fill the East tank with an energizing blend to rise, and the West tank with a relaxing blend for rest.",
+        price: "$189.95"
     },
 
     gold: {
         productpic: "diffuser-gold.jpg",
         name: "AROMA OM GOLD MARBLE DIFFUSER",
         slogan: "Find your balance.",
-        features: "100 ml water tank that diffuses approximately 400 sq ft. Mists for approximately 3 hours continuous | 6 hours intermittent (30 sec on/30 sec off). Sleek polyresin cover and an optional LED light feature. Automatic shut-off and whisper-quiet operation."
+        features: "100 ml water tank that diffuses approximately 400 sq ft. Mists for approximately 3 hours continuous | 6 hours intermittent (30 sec on/30 sec off). Sleek polyresin cover and an optional LED light feature. Automatic shut-off and whisper-quiet operation.",
+        price: "$89.95"
     },
 };
 
