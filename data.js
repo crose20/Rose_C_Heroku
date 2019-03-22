8c87cb0ee2ebc481aed924a72db1e9471b43c552
Carter Rose <CarterRose@Carters-MacBook-Pro.local>
2019-03-22 13:06:32 -0400

    last commit?

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ const data = {
         slogan: "Shine brightly, breathe deeply.",
         features: "115 ml water tank that diffuses approximately 450 sq ft. Mists for approximately 4.5 hours continuous | 9 hours intermittent (30 sec on/30 sec off). Decorative glass cover. Automatic shut-off and whisper-quiet operation.",
         price: "$79.95",
-        benefits: "BENEFITS:<br><br>Purifies and humidifies while infusing your air with the healing power of plants.<br><br>Releases negative ions to reduce dust, pet dander and other airborne allergens (like dust mite matter).<br><br>Refreshes the air in your space using 100% natural essential oil blends.<br><br>Preserves the full integrity and properties of your essential oil blends using a heat-free system.<br><br>Gives your atmosphere the same refreshing feeling as standing near a waterfall."
+        benefits: "BENEFITS: Purifies and humidifies while infusing your air with the healing power of plants. Releases negative ions to reduce dust, pet dander and other airborne allergens (like dust mite matter). Refreshes the air in your space using 100% natural essential oil blends. Preserves the full integrity and properties of your essential oil blends using a heat-free system. Gives your atmosphere the same refreshing feeling as standing near a waterfall."
     },
 
     stone: {
@@ -15,7 +15,7 @@ const data = {
         slogan: "Your wellness is etched in stone.",
         features: "Heat-free to preserve the integrity of your oils for maximum benefit. May also be used as a humidifier. Soft LED light. Energy-efficient, whisper-quiet operation that features an auto shut-off option. Decorative cement cover.",
         price: "$89.95",
-        benefits: "BENEFITS:<br><br>Purifies and humidifies while infusing your air with the healing power of plants.<br><br>Releases negative ions to reduce dust, pet dander and other airborne allergens (like dust mite matter).<br><br>Refreshes the air in your space using 100% natural essential oil blends.<br><br>Preserves the full integrity and properties of your essential oil blends using a heat-free system.<br><br>Gives your atmosphere the same refreshing feeling as standing near a waterfall."
+        benefits: "BENEFITS: Purifies and humidifies while infusing your air with the healing power of plants. Releases negative ions to reduce dust, pet dander and other airborne allergens (like dust mite matter). Refreshes the air in your space using 100% natural essential oil blends. Preserves the full integrity and properties of your essential oil blends using a heat-free system. Gives your atmosphere the same refreshing feeling as standing near a waterfall."
     },
 
     time: {
@@ -24,7 +24,7 @@ const data = {
         slogan: "It's about time.",
         features: "Two tanks and a timer you can pre-set to start automatically morning and night means you can have wellness around the clock. Inspired by the body’s natural rhythms, fill the East tank with an energizing blend to rise, and the West tank with a relaxing blend for rest.",
         price: "$189.95",
-        benefits: "BENEFITS:<br><br>Purifies and humidifies while infusing your air with the healing power of plants.<br><br>Releases negative ions to reduce dust, pet dander and other airborne allergens (like dust mite matter).<br><br>Refreshes the air in your space using 100% natural essential oil blends.<br><br>Preserves the full integrity and properties of your essential oil blends using a heat-free system.<br><br>Gives your atmosphere the same refreshing feeling as standing near a waterfall."
+        benefits: "BENEFITS: Purifies and humidifies while infusing your air with the healing power of plants. Releases negative ions to reduce dust, pet dander and other airborne allergens (like dust mite matter). Refreshes the air in your space using 100% natural essential oil blends. Preserves the full integrity and properties of your essential oil blends using a heat-free system. Gives your atmosphere the same refreshing feeling as standing near a waterfall."
     },
 
     gold: {
@@ -33,7 +33,7 @@ const data = {
         slogan: "Find your balance.",
         features: "100 ml water tank that diffuses approximately 400 sq ft. Mists for approximately 3 hours continuous | 6 hours intermittent (30 sec on/30 sec off). Sleek polyresin cover and an optional LED light feature. Automatic shut-off and whisper-quiet operation.",
         price: "$89.95",
-        benefits: "BENEFITS:<br><br>Purifies and humidifies while infusing your air with the healing power of plants.<br><br>Releases negative ions to reduce dust, pet dander and other airborne allergens (like dust mite matter).<br><br>Refreshes the air in your space using 100% natural essential oil blends.<br><br>Preserves the full integrity and properties of your essential oil blends using a heat-free system.<br><br>Gives your atmosphere the same refreshing feeling as standing near a waterfall."
+        benefits: "BENEFITS: Purifies and humidifies while infusing your air with the healing power of plants. Releases negative ions to reduce dust, pet dander and other airborne allergens (like dust mite matter). Refreshes the air in your space using 100% natural essential oil blends. Preserves the full integrity and properties of your essential oil blends using a heat-free system. Gives your atmosphere the same refreshing feeling as standing near a waterfall."
     },
 };
 
