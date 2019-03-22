0331a3fbea966a85306d20fdedf93e0f06479ba6
Carter Rose <CarterRose@Carters-MacBook-Pro.local>
2019-03-22 13:41:53 -0400

    one last commit

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const data = require('../data');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Saje Diffusers', message: 'Invite the power of nature into your décor with an essential oil diffuser. Fill any space, big or small with wellness-enhancing oil blends.' });
+  res.render('index', { title: 'Saje Diffusers', message: 'Invite the power of nature into your décor with an essential oil diffuser. Fill any space, big or small with wellness-enhancing oil blends.', whyuseone: 'WHY USE ONE? Diffusing allows you to bring nature’s abundant healing properties into your home, office, or anywhere you choose to diffuse. If youre looking for more calm, more rest, more energy or more focus, theres a diffuser blend to help with that.' });
   
 });
 
